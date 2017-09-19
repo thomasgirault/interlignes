70486e1cd1390263fa5651aae982bae6588ae167
Thomas Girault <thomas.girault@ouest-france.fr>
2017-09-19 19:22:19 +0200

    simplify GUI

--- visualisation/js/gui.js
+++ visualisation/js/gui.js
@@ -1,80 +1,38 @@
-var gui = new dat.GUI();
+class Controls{    
+    constructor(params) {        
+        this.gui = new dat.GUI();
+        var stp = 1;
+        for(var name in params){
+            var p = params[name];
+            this.add_param(name, p["val"], p["min"], p["max"], stp);
+        }
+    }
+    
+    add_param(n, val, min_, max_, stp){
+        var o = {};
+        o[n] = val;       
+        this.gui.add(o, n, min_, max_).onChange( function(value) {
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8888/param/" + n + "/" + Math.round(value),
+                data: JSON.stringify({n: Math.round(value)}),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data){console.log(data);},
+                failure: function(errMsg) {
+                    alert(errMsg);
+                }
+            });
+        });
+    }
+}
 
-gui.add({"min_depth":33}, 'min_depth', 0, 255).step(1).onChange( function(value) {
-    console.log(value);
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8888/param/min_depth/" + value,
-      data: JSON.stringify({"min_depth": value}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data){console.log(data);},
-      failure: function(errMsg) {
-          alert(errMsg);
-      }
-  });
-});
+params = {"min_depth":{"val":0, "min":0, "max":255},
+         "max_depth":{"val":255, "min":0, "max":255},
+         "theta":{"val":0, "min":0, "max":255}, 
+         "MAX_DIST":{"val":40, "min":1, "max":70}, 
+         "save":{"val":false, "min":false, "max":true}};
 
-gui.add({"max_depth":40}, 'max_depth', 0, 255).step(1).onChange( function(value) {
-    console.log(value);
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8888/param/max_depth/" + value,
-      data: JSON.stringify({"max_depth": value}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data){console.log(data);},
-      failure: function(errMsg) {
-          alert(errMsg);
-      }
-  });
-});
-
-
-
-gui.add({"theta":10}, 'theta', 0, 255).step(1).onChange( function(value) {
-    console.log(value);
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8888/param/theta/" + value,
-      data: JSON.stringify({"theta": value}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data){console.log(data);},
-      failure: function(errMsg) {
-          alert(errMsg);
-      }
-  });
-});
-
-
-gui.add({"save":0}, 'save', 0, 2).step(1).onChange( function(value) {
-    console.log(value);
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8888/param/save/" + value,
-      data: JSON.stringify({"save": value}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data){console.log(data);},
-      failure: function(errMsg) {
-          alert(errMsg);
-      }
-  });
-});
-
-
-gui.add({"MAX_DIST":0}, 'MAX_DIST', 0, 18750).step(256).onChange( function(value) {
-    console.log(value);
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8888/param/MAX_DIST/" + value,
-      data: JSON.stringify({"MAX_DIST": value}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data){console.log(data);},
-      failure: function(errMsg) {
-          alert(errMsg);
-      }
-  });
-});
+window.onload = function(){          
+    var c = new Controls(params);
+};
