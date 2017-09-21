53b9e5989bff86e5a82f8a7303b017edce72b8a1
Thomas Girault <thomas.girault@ouest-france.fr>
2017-09-21 14:42:39 +0200

    Ajout de boutons dans la GUI

--- visualisation/js/gui.js
+++ visualisation/js/gui.js
@@ -1,16 +1,39 @@
 class Controls{    
     constructor(params) {        
         this.gui = new dat.GUI();
+        this.gui.remember(params);
+        
         var stp = 1;
         for(var name in params){
             var p = params[name];
-            this.add_param(name, p["val"], p["min"], p["max"], stp);
+            if (!p.hasOwnProperty('val')) 
+                this.add_button(name);     
+            else
+                this.add_param(name, p["val"], p["min"], p["max"], stp);
         }
     }
     
+    add_button(name){
+        var obj = {learnBG:
+            function(){
+                $.ajax({
+                    type: "POST",
+                    url: "http://localhost:8888/param/" + name + "/" + Math.round(1),
+                    data: JSON.stringify({name: Math.round(0)}),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function(data){console.log(data);},
+                    failure: function(errMsg) {alert(errMsg);}
+                });
+            }   
+        };
+        this.gui.add(obj,"learnBG");
+    }
+
     add_param(n, val, min_, max_, stp){
         var o = {};
-        o[n] = val;       
+        o[n] = val;
+         
         this.gui.add(o, n, min_, max_).onChange( function(value) {
             $.ajax({
                 type: "POST",
@@ -19,9 +42,7 @@ class Controls{
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 success: function(data){console.log(data);},
-                failure: function(errMsg) {
-                    alert(errMsg);
-                }
+                failure: function(errMsg) {alert(errMsg);}
             });
         });
     }
@@ -30,8 +51,14 @@ class Controls{
 params = {"min_depth":{"val":0, "min":0, "max":255},
          "max_depth":{"val":255, "min":0, "max":255},
          "theta":{"val":0, "min":0, "max":255}, 
-         "MAX_DIST":{"val":40, "min":1, "max":70}, 
-         "save":{"val":false, "min":false, "max":true}};
+         "MAX_DIST":{"val":40, "min":1, "max":70},
+         "min_blob_size":{"val":30, "min":1, "max":300},
+         "max_blob_size":{"val":500, "min":100, "max":500},
+         "min_norm":{"val":1, "min":0, "max":10},
+         "extra_spaces":{"val":1, "min":0, "max":20},
+         "save":{"val":false, "min":false, "max":true},
+         "learnBG":{"min":null, "max":null}
+        };
 
 window.onload = function(){          
     var c = new Controls(params);
