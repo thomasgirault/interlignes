f0413769c3695137cc17fee78c50b3e735c87f28
Thomas Girault <thomas.girault@ouest-france.fr>
2017-09-25 19:28:16 +0200

    clean code and comments

--- visualisation/js/main_texter.js
+++ visualisation/js/main_texter.js
@@ -1,13 +1,12 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+Maptastic("canvas");
 
-var tracker = new WebSocket("ws://127.0.0.1:8888/tracker");
-// var corpus = new WebSocket("ws://127.0.0.1:8888/corpus");
 
 
 var texters = []
 var nb_texters = 30;
 var lastIndex = 0;
-
-// activer et desactiver les texters en fonction du tracking en cours 
 
 for (i = 0; i < nb_texters; i++) {
 	var t = new Texter(i);
@@ -16,46 +15,33 @@ for (i = 0; i < nb_texters; i++) {
 }
 
 
-// corpus.onmessage = function (event) {
-// 	// il suffit maintenant que texter notifie qu'il arrive en textIndex == newText.length;
-// 	// on fera alors une requete ajax au serveur pour avoir un nouveau texte
-// 	console.log(event.data);
-// 	texters[lastIndex].text = event.data;
-// 	texters[lastIndex].textIndex = 0;
-// 	lastIndex++;
-// 	lastIndex %= nb_texters;
-// };
-
-canvas = document.getElementById('canvas');
-context = canvas.getContext('2d');
-Maptastic("canvas");
-
+var tracker = new WebSocket("ws://127.0.0.1:8888/tracker");
 
 tracker.onmessage = function (event) {
+	// activer et desactiver les texters en fonction du tracking en cours 
 	if (typeof event.data === 'string' || event.data instanceof String) {
 		$.each($.parseJSON(event.data), function (index, value) {
 			var i = parseInt(index) % nb_texters;
 			console.log(value);
 			texters[i].tracked_point = [(1920. / 512.) * value[0], (1080 / 424.) * value[1]];
 		});
-	}
-	// for (i = 0; i < nb_texters; i++) {
-	// 	texters[i].tracked_point = [points[i][0], points[i][1]];
-	// }
-	// context.save();
-	// context.fillStyle = 'rgba(0,255,0,0.1)';
-	// context.fillRect(texters[i].tracked_point[0], texters[i].tracked_point[0], 10, 10);
-	// context.restore();
-
+	}	
 };
+
+
+
+
+
 
 var frame = 0;
 
 function clear() {
 	if (frame == 0) {
+		context.save();
 		context.fillStyle = 'rgba(0,0,0,0.1)';
 		context.fillRect(0, 0, canvas.width, canvas.height);
-		context.fillStyle = 'rgba(255,255,255,1)';
+		// context.fillStyle = 'rgba(255,255,255,1)';
+		context.restore();
 	}
 	frame++;
 	frame %= 20;
