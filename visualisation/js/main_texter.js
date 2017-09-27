9ba50dd213f9001ff38a3172c292f3f146738c3b
Thomas Girault <thomas.girault@ouest-france.fr>
2017-09-27 15:06:16 +0200

    intro video

--- visualisation/js/main_texter.js
+++ visualisation/js/main_texter.js
@@ -1,7 +1,14 @@
+var video = $("#interlude")[0];
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-Maptastic("canvas");
 
+// var bufferCanvas = document.getElementById('buffer');
+// var buffer = bufferCanvas.getContext('2d');
+var width = 1920, height = 1080;
+
+
+
+Maptastic("canvas");
 
 
 var texters = []
@@ -17,51 +24,75 @@ for (i = 0; i < nb_texters; i++) {
 
 var tracker = new WebSocket("ws://127.0.0.1:8888/tracker");
 
+var height_ratio = (height / 424.);
+var width_ratio = (width / 512.);
+
+
 tracker.onmessage = function (event) {
 	// activer et desactiver les texters en fonction du tracking en cours 
 	if (typeof event.data === 'string' || event.data instanceof String) {
 		$.each($.parseJSON(event.data), function (index, value) {
 			var i = parseInt(index) % nb_texters;
-			console.log(value);
-			texters[i].tracked_point = [(1920. / 512.) * value[0], (1080 / 424.) * value[1]];
+			texters[i].tracked_point = [width_ratio * value[0], height_ratio * value[1]];
 		});
-	}	
+	}
 };
 
 
+function processFrame() {
+	buffer.drawImage(video, 0, 0, width, height);
 
+	// this can be done without alphaData, except in Firefox which doesn't like it when image is bigger than the canvas
+	var image = buffer.getImageData(0, 0, width, height),
+		imageData = image.data,
+		alphaData = buffer.getImageData(0, height, width, height).data;
 
+	for (var i = 3, len = imageData.length; i < len; i = i + 4) {
+		imageData[i] = alphaData[i - 1];
+	}
+
+	context.putImageData(image, 0, 0, 0, 0, width, height);
+}
 
 
 var frame = 0;
-
 function clear() {
 	if (frame == 0) {
 		context.save();
 		context.fillStyle = 'rgba(0,0,0,0.1)';
 		context.fillRect(0, 0, canvas.width, canvas.height);
-		// context.fillStyle = 'rgba(255,255,255,1)';
+		context.fillStyle = 'rgba(255,255,255,1)';
 		context.restore();
 	}
 	frame++;
 	frame %= 20;
 }
 
-
 function init() {
 	clear();
 	raf = window.requestAnimationFrame(init);
 }
 
+video.addEventListener("play", draw_video, false);
+function draw_video() {
+	if (video.paused || video.ended) {
+		return;
+	}
+	// processFrame();
+	context.drawImage(video, 0, 0, width, height);
+	requestAnimationFrame(draw_video);
+}
 
-var gui = new dat.GUI();
-gui.add(texters[0], 'text').name('Text').onChange(function () { texters[0].onTextChange() });
-gui.add(texters[0], 'minFontSize', 3, 100).name('Minimum Size');
-gui.add(texters[0], 'maxFontSize', 3, 400).name('Maximum Size');
-gui.add(texters[0], 'angleDistortion', 0, 2).step(0.1).name('Random Angle');
-// gui.addColosr(texter, 'textColor').name( 'Text Color' ).onChange( function(value) { texter.applyNewColor( value ) } );
-// gui.addColosr(texter, 'bgColor').name( 'Background Color' ).onChange( function(value) { texter.setBackground( value ) } );
-gui.add(texters[0], 'clear').name('Clear');
-gui.add(texters[0], 'save').name('Save');
 init();
+// draw_video();
+
+// var gui = new dat.GUI();
+// gui.add(texters[0], 'text').name('Text').onChange(function () { texters[0].onTextChange() });
+// gui.add(texters[0], 'minFontSize', 3, 100).name('Minimum Size');
+// gui.add(texters[0], 'maxFontSize', 3, 400).name('Maximum Size');
+// gui.add(texters[0], 'angleDistortion', 0, 2).step(0.1).name('Random Angle');
+// // gui.addColosr(texter, 'textColor').name( 'Text Color' ).onChange( function(value) { texter.applyNewColor( value ) } );
+// // gui.addColosr(texter, 'bgColor').name( 'Background Color' ).onChange( function(value) { texter.setBackground( value ) } );
+// gui.add(texters[0], 'clear').name('Clear');
+// gui.add(texters[0], 'save').name('Save');
 
--- visualisation/js/texter.js
+++ visualisation/js/texter.js
@@ -1,187 +1,251 @@
-/*
- *  Texter - Drawing with Text.
- *  - Ported from demo in Generative Design book - http://www.generative-gestaltung.de
- *  - generative-gestalung.de original licence: http://www.apache.org/licenses/LICENSE-2.0
- *
- *  - Modified and maintained by Tim Holman - tholman.com - @twholman
- */
+var defaultText = 'Texter ';
 
 function Texter(id) {
 
-  var _this = this;
+    var _this = this;
 
-  // Application variables
-  position = { x: 0, y: window.innerHeight / 2 };
-  this.id = id;
-  this.textIndex = 0;
-  this.textColor = "#ffffff";
-  this.bgColor = "#000000";
-  this.minFontSize = 30;
-  this.maxFontSize = 300;
-  this.angleDistortion = 0.00;
-  this.tracked_point = [0, 0];
-  this.last_tracked_point = [0, 0];
+    // Application variables
+    this.textIndex = 0;
+    this.id = id;
+    this.textColor = "#ffffff";
+    this.bgColor = "#000000";
+    this.curFontSize = 12;
+    this.varFontSize = 0.4;
+    this.angle = 0;
+    this.angleDelta = 0;
+    this.angleDistortion = 0.01;
+    this.completeWords = true;
+    this.tracked_point = [0, 0];
+    this.last_tracked_point = [0, 0];
 
+    this.text = "";
 
-  this.text = "Un flic à vélo gare son vélo et entre dans le tabac ; il en ressort presque aussitôt, on ne sait pas ce qu'il a acheté (des cigarettes ? un stylo à bille , un timbre , des cachous , un paquet de mouchoirs en papier ?)";
+    // Drawing Variables
+    canvas = null;
+    context = null;
 
-  // Drawing Variables
-  canvas = null;
-  context = null;
+    this.initialize = function () {
 
-  bgCanvas = null;
-  bgContext = null;
-
-  this.initialize = function () {
-
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-
-    bgCanvas = document.createElement('canvas');
-    bgContext = bgCanvas.getContext('2d');
-    bgCanvas.width = canvas.width;
-    bgCanvas.height = canvas.height;
-    _this.setBackground(_this.bgColor);
-    context.fillStyle = _this.textColor;
+        canvas = document.getElementById('canvas');
+        context = canvas.getContext('2d');
+        canvas.width = 1920;
+        canvas.height = 1080;
+        _this.setBackground(_this.bgColor);
+        context.fillStyle = _this.textColor;
+        update();
+        askNewText();
+    };
 
 
-    window.onresize = function (event) {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      bgCanvas.width = window.innerWidth;
-      bgCanvas.height = window.innerHeight;
-      _this.setBackground(_this.bgColor);
-      _this.clear();
+    function update() {
+        requestAnimationFrame(update);
+        draw();
     }
 
-    update();
-    askNewText();
-  };
+    function draw() {
+        if (_this.tracked_point != _this.last_tracked_point) {
+            var newDistance = dist(_this.tracked_point, _this.last_tracked_point);
+            var fontSize = calcFontSize(newDistance);
+            var letter = _this.text[_this.textIndex];
+            var stepSize = textWidth(letter, fontSize);
+
+            if (newDistance > stepSize) {
+                var angle = Math.atan2(_this.tracked_point[1] - _this.last_tracked_point[1], _this.tracked_point[0] - _this.last_tracked_point[0]);
+                _this.angleDelta = _this.angle - angle;
+                if (_this.angleDelta > 3.14) {
+                    _this.angleDelta = _this.angle + angle;
+                }
+                _this.angle = angle;
+
+                context.font = fontSize + "px Georgia";
+
+                letter_to_context(letter);
+
+                _this.textIndex++;
+                if (_this.textIndex > _this.text.length - 1) {
+                    _this.textIndex = 0;
+                    askNewText();
+                }
+
+                _this.last_tracked_point[0] += Math.cos(angle) * stepSize;
+                _this.last_tracked_point[1] += Math.sin(angle) * stepSize;
+
+            }
+        }
+    };
 
 
 
-  var update = function () {
-    requestAnimationFrame(update);
-    draw();
-  }
-
-  var draw = function () {
-    if (_this.tracked_point != _this.last_tracked_point) {
-      position.x = _this.last_tracked_point[0];
-      position.y = _this.last_tracked_point[1];
-      _this.last_tracked_point = _this.tracked_point;
-
-
-      var newDistance = distance(position, { x: _this.tracked_point[0], y: _this.tracked_point[1] });
-
-      var fontSize = _this.minFontSize + newDistance / 2;
-
-      if (fontSize > _this.maxFontSize) {
-        fontSize = _this.maxFontSize;
-      }
-
-      var letter = _this.text[_this.textIndex];
-      var stepSize = textWidth(letter, fontSize);
-
-      if (newDistance > stepSize) {
-        var angle = Math.atan2(_this.tracked_point[1] - position.y, _this.tracked_point[0] - position.x);
-
+    function finishWord() {
+        var newDistance = dist(_this.tracked_point, _this.last_tracked_point);
+        var fontSize = calcFontSize(newDistance);
+        var letter = _this.text[_this.textIndex];
         context.font = fontSize + "px Georgia";
+        while (letter != ' ') {
 
+            console.log("Finish word");
+            letter_to_context(letter);
+
+            _this.textIndex++;
+            if (_this.textIndex > _this.text.length - 1) {
+                _this.textIndex = 0;
+                // askNewText();
+                return;
+            }
+            else {
+                var stepSize = textWidth(letter, fontSize);
+                _this.last_tracked_point[0] += Math.cos(angle) * stepSize;
+                _this.last_tracked_point[1] += Math.sin(angle) * stepSize;
+                letter = _this.text[_this.textIndex];
+                _this.angle -= _this.angleDelta;
+                _this.angleDelta -= _this.angleDelta / 6;
+            }
+        }
+    }
+
+    function dist(pt, pt2) {
+        var xs = pt2[0] - pt[0];
+        var ys = pt2[1] - pt[1];
+        return Math.sqrt(xs * xs + ys * ys);
+    };
+
+
+
+    function calcFontSize(d) {
+        var fontSize = _this.curFontSize + d * _this.varFontSize;
+        return fontSize;
+    }
+
+    function letter_to_context(letter) {
         context.save();
-        context.translate(position.x, position.y);
-        context.rotate(angle + (Math.random() * (_this.angleDistortion * 2) - _this.angleDistortion));
+        context.translate(_this.last_tracked_point[0], _this.last_tracked_point[1]);
+        context.rotate(_this.angle + (Math.random() * (_this.angleDistortion * 2) - _this.angleDistortion));
         context.fillText(letter, 0, 0);
         context.restore();
+    }
 
 
 
-        _this.textIndex++;
-        if (_this.textIndex > _this.text.length - 1) {
-          _this.textIndex = 0;
-          askNewText()
+
+    function textWidth(string, size) {
+        context.font = size + "px Georgia";
+
+        if (context.fillText) {
+            return context.measureText(string).width;
+
+        } else if (context.mozDrawText) {
+            return context.mozMeasureText(string);
+
         }
+    };
 
-        position.x = position.x + Math.cos(angle) * stepSize;
-        position.y = position.y + Math.sin(angle) * stepSize;
-
-      }
+    this.clear = function () {
+        canvas.width = canvas.width;
+        textIndex = 0;
+        context.fillStyle = _this.textColor;
     }
-  };
 
-  var distance = function (pt, pt2) {
-    var xs = 0;
-    var ys = 0;
-    xs = pt2.x - pt.x;
-    ys = pt2.y - pt.y;
-    return Math.sqrt(xs * xs + ys * ys);
-  };
-
-
-  var textWidth = function (string, size) {
-    context.font = size + "px Georgia";
-    // "24pt 'Times New Roman'"
-
-    if (context.fillText) {
-      return context.measureText(string).width;
-
-    } else if (context.mozDrawText) {
-      return context.mozMeasureText(string);
-
+    this.applyNewColor = function (value) {
+        _this.textColor = value;
+        context.fillStyle = _this.textColor;
     }
-  };
 
-  this.clear = function () {
-    canvas.width = canvas.width;
-    context.fillStyle = _this.textColor;
-  }
+    this.setBackground = function (value) {
+        _this.bgColor = value;
+        canvas.style.backgroundColor = value;
 
-  this.applyNewColor = function (value) {
-    _this.textColor = value;
-    context.fillStyle = _this.textColor;
-  }
+    };
 
-  this.setBackground = function (value) {
-    _this.bgColor = value;
-    canvas.style.backgroundColor = value;
-
-  };
-
-  this.onTextChange = function () {
-    _this.textIndex = 0;
-  }
-
-  this.save = function () {
-    // Prepare the background canvas's color
-    bgContext.rect(0, 0, bgCanvas.width, bgCanvas.height);
-    bgContext.fillStyle = _this.bgColor;
-    bgContext.fill();
-
-    // Draw the front canvas onto the bg canvas
-    bgContext.drawImage(canvas, 0, 0);
-    // Open in a new window
-    window.open(bgCanvas.toDataURL('image/png'), 'mywindow');
-  }
+    this.setText = function (text) {
+        if (text == '') {
+            text = defaultText;
+        }
+        this.text = text;
+        textIndex = 0;
+    }
 
 
-  function askNewText() {
-    console.log(_this.id, "asks new text");
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8888/paragraphe/" + id,
-      data: JSON.stringify({ 'id': _this.id}),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function (data) { 
-        console.log(data); 
-        _this.text = data.texte;  
-      },
-      failure: function (errMsg) { alert(errMsg); }
-    });
-  }
 
+    function askNewText() {
+        console.log(_this.id, "asks new text");
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8888/paragraphe/" + id,
+            data: JSON.stringify({ 'id': _this.id }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                _this.text = data.texte;
+            },
+            failure: function (errMsg) { alert(errMsg); }
+        });
+    }
 
 };
+
+
+
+
+// this.save = function () {
+
+//     // Prepare the background canvas's color
+//     bgContext.rect(0, 0, bgCanvas.width, bgCanvas.height);
+//     bgContext.fillStyle = _this.bgColor;
+//     bgContext.fill();
+
+//     // Draw the front canvas onto the bg canvas
+//     bgContext.drawImage(canvas, 0, 0);
+
+//     // Open in a new window
+//     window.open(bgCanvas.toDataURL('image/png'), 'mywindow');
+
+// }
+
+
+    // var mouseUp = function (event) {
+    //     if (mouse.down == false) {
+    //         return;
+    //     }
+    //     mouse.down = false;
+    //     if (_this.completeWords == false) {
+    //         return;
+    //     }
+
+    //     // Finish word following the same angle
+    //     var newDistance = distance(position, mouse);
+    //     var fontSize = calcFontSize(newDistance);
+    //     var letter = _this.text[textIndex];
+    //     context.font = fontSize + "px Gabriela";
+    //     while (letter != ' ') {
+
+    //         letter_to_context(letter);
+
+    //         textIndex++;
+    //         if (textIndex > _this.text.length - 1) {
+    //             textIndex = 0;
+    //             return;
+    //         }
+    //         else {
+    //             var stepSize = textWidth(letter, fontSize);
+    //             position.x = position.x + Math.cos(_this.angle) * stepSize;
+    //             position.y = position.y + Math.sin(_this.angle) * stepSize;
+    //             letter = _this.text[textIndex];
+    //             _this.angle -= _this.angleDelta;
+    //             _this.angleDelta -= _this.angleDelta / 6;
+    //         }
+    //     }
+    // }
+
+
+
+            // mouse = { x: 0, y: 0, down: false };
+
+        // bgCanvas = null;
+        // bgContext = null;
+
+        // bgCanvas = document.createElement('canvas');
+        // bgContext = bgCanvas.getContext('2d');
+        // bgCanvas.width = canvas.width;
+        // bgCanvas.height = canvas.height;
+    //position = {x: 0, y: window.innerHeight/2};
