cd2fcb7596c62b9cca2023e543c5c9731cdc07f0
Thomas Girault <thomas.girault@ouest-france.fr>
2017-10-07 11:03:20 +0200

    adds button to save current parameters

--- visualisation/js/gui.js
+++ visualisation/js/gui.js
@@ -93,6 +93,23 @@ var params = {
 };
 
 
+var web_params = {
+    //  "width_ratio":{"val":800, "min":1920, "max":1920},
+    //  "heigth_ratio":{"val":2.55, "min":1, "max":5},
+    "minFontSize": { "val": 10, "min": 3, "max": 100 },
+    "maxFontSize": { "val": 40, "min": 15, "max": 300 },
+    "clearPeriod": { "val": 5, "min": 0, "max": 100 },
+    "max_word_ts_interval": { "val": 2, "min": 0, "max": 10 },
+    "kerning": { "val": 0, "min": 0, "max": 100 },
+    "min_distance": { "val": 10, "min": 0, "max": 50 },
+    "interlude": { "val": 0, "min": 0, "max": 16 },
+    "interlude_play": { "val": false, "min": false, "max": true }
+};
+
+
+
+// SAVE PARAMS
+
 $.ajax({
     type: "GET",
     async: false,
@@ -108,28 +125,53 @@ $.ajax({
         });
     },
     failure: function (errMsg) { alert(errMsg); }
-})
+});
+
+
+
+$.ajax({
+    type: "GET",
+    async: false,
+    url: "http://localhost:8888/text_params.json",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (data) {
+        $.each(data, function (key, value) {
+            if(key in params){
+                console.log(key, params[key]);
+                web_params[key]["val"] = value;
+            }
+        });
+    },
+    failure: function (errMsg) { alert(errMsg); }
+});
+
+
+
+
+function save_all(gui){
+        var obj = {
+            save_params:
+            function () {
+                $.ajax({
+                    type: "GET",
+                    url: "/save_params",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (data) { console.log(data); },
+                    failure: function (errMsg) { alert(errMsg); }
+                });
+            }
+        };
+        gui.add(obj, "save_params");
+
+}
 
 
 
 
 
 
-
-
-var web_params = {
-    //  "width_ratio":{"val":800, "min":1920, "max":1920},
-    //  "heigth_ratio":{"val":2.55, "min":1, "max":5},
-
-    "minFontSize": { "val": 10, "min": 3, "max": 100 },
-    "maxFontSize": { "val": 40, "min": 15, "max": 300 },
-    "clearPeriod": { "val": 5, "min": 0, "max": 100 },
-    "max_word_ts_interval": { "val": 2, "min": 0, "max": 10 },
-    "kerning": { "val": 0, "min": 0, "max": 100 },
-    "min_distance": { "val": 10, "min": 0, "max": 50 },
-    "interlude1": { "val": false, "min": false, "max": true },
-    "interlude2": { "val": false, "min": false, "max": true }
-};
 
 
 window.onload = function () {
@@ -144,7 +186,10 @@ window.onload = function () {
     var url_web_params = "/web_param";
     var c2 = new Controls(gui, "texte", web_params, url_web_params);
 
+    save_all(gui);
 };
+
+
 
 
     // if(typeof(Storage) !== "undefined") {
