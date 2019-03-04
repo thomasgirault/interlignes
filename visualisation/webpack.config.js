c9476d63663802db15da4a71e81fc8b2a4dd3991
Thomas Girault <thomas@girault.fr>
2019-03-04 07:40:18 +0100

    updates content

--- visualisation/webpack.config.js
+++ visualisation/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const jquery = require('jquery')
+
 
 module.exports = {
   mode: 'development',
